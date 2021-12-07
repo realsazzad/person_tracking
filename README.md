880fa526d721783d7b3f87167f135a07059b7ea5
realsazzad <realsazzad@gmail.com>
2021-12-07 12:18:28 +0900

    Code updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# person_tracking
-A simple program that tracks persons in a video by the color of the shirt/jersey they are wearing (in this case Black)
+A simple program that tracks persons in a video by the color of the shirt/jersey they are wearing (in this case Black) using OpenCV (Python)
+
+1. Install necessary libraries
+2. To run the program, execute main.py (provide the video file path in the argument)
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import glob
 from person_tracking import person_tracker
 
 
-def frames_to_video(frames_path):
+def frames_to_video(frames_path, video_save_path):
+#This function creates a video from the frames saved in the path given as argument and
+#save the created video in video_save_path
     img_array = []
     for image_file in glob.glob(frames_path):
         img = cv2.imread(image_file)
@@ -11,7 +13,7 @@ def frames_to_video(frames_path):
         size = (width, height)
         img_array.append(img)
 
-    out = cv2.VideoWriter('basket_result.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+    out = cv2.VideoWriter(video_save_path, cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
 
     for i in range(len(img_array)):
         out.write(img_array[i])
@@ -19,7 +21,14 @@ def frames_to_video(frames_path):
 
 if __name__ == '__main__':
 
-    person_tracker('basket_test.mp4')
+    input_video_path = 'basket_test.mp4'
+    output_video_path = 'basket_result.avi'
+    output_frames_path = 'output/*.jpg'
+
+    person_tracker(input_video_path)
+    frames_to_video(output_frames_path, output_video_path)
+
+
 
 
 
